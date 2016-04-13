2faef37ec559caf8839e0e38a0647759cf43af9f
Kunal Patel <kunalpatelx92@gmail.com>
2016-04-13 12:46:18 -0500

    Update hamsters.rb file post pairing

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,89 +1,41 @@
-# Release 1: Gather Data on Hamsters of Unknown Origin
+# 4.3 Mandatory Pairing: Data Types, IRB, and User Input
 
-def hamster_info
-	puts "Hamster Name:"
-	p hamster_name = gets.chomp
+puts "Hamster Name:"
+hamster_name = gets.chomp
 
-	puts "Volume Level:"
-	p volume_level = gets.chomp
+puts "Volume Level:"
+volume_level = gets.chomp
+volume_level = volume_level.to_i
 
-	puts "Fur Color:"
-	p fur_color = gets.chomp
+puts "Fur Color:"
+fur_color = gets.chomp
 
-	puts "Good Candidate?"
-	p good_candidate = gets.chomp
+puts "Good Candidate? (y/n)"
+good_candidate = gets.chomp
 
-	puts "Estimated Age:"
-	p estimated_age = gets.chomp
+puts "Estimated Age:"
+estimated_age = gets.chomp
+estimated_age = estimated_age.to_i
+p estimated_age
+
+puts "\nThis hamster's name is #{hamster_name}."
+
+if volume_level >= 5
+	puts "This hamster is loud and has a volume of #{volume_level}."
+else
+	puts "This hamster is quiet and has a volume of #{volume_level}."
 end
 
-# Release 2: Process String Input
+puts "This hamster has a #{fur_color} fur color."
 
-def hamster_info
-	puts "Hamster Name:"
-	p hamster_name = gets.chomp
-
-	puts "Volume Level:"
-	volume_level = gets.chomp
-	p volume_level = volume_level.to_i
-
-	puts "Fur Color:"
-	p fur_color = gets.chomp
-
-	puts "Good Candidate? (y/n)"
-	good_candidate = gets.chomp
-		if good_candidate == "y"
-			p true
-		else
-			p false
-		end
-
-	puts "Estimated Age:"
-	estimated_age = gets.chomp
-		if estimated_age.empty?
-			p nil
-		else
-			p estimated_age.to_i
-		end
+if good_candidate == "y"
+	puts "This hamster is a great hamster!"
+else
+	puts "This hamster is a problem hamster!"
 end
 
-# Release 3: Print to the Console
-
-def hamster_info
-	puts "Hamster Name:"
-	hamster_name = gets.chomp
-	puts "This hamster's name is #{hamster_name}"
-
-	puts "Volume Level:"
-	volume_level = gets.chomp
-	volume_level = volume_level.to_i
-		if volume_level >= 5
-			puts "This hamster is loud and has a volume of #{volume_level}"
-		else
-			puts "This hamster is quiet and has a volume of #{volume_level}"
-		end
-
-	puts "Fur Color:"
-	fur_color = gets.chomp
-	puts "This hamster has a #{fur_color} fur color"
-
-	puts "Good Candidate? (y/n)"
-	good_candidate = gets.chomp
-		if good_candidate == "y"
-			puts "This hamster is a great hamster!"
-		else
-			puts "This hamster is a problem hamster!"
-		end
-
-	puts "Estimated Age:"
-	estimated_age = gets.chomp
-		if estimated_age.empty?
-			p nil
-			puts "This hamster's age is undetermined"
-		else
-			p estimated_age.to_i
-			puts "This hamster is #{estimated_age} years old."
-		end
+if estimated_age.zero?
+	puts "This hamster's age is undetermined."
+else
+	puts "This hamster is #{estimated_age} years old."
 end
-
-hamster_info()
