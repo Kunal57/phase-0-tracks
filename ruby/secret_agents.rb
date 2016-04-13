2bb976f3e860fdd13942ed823836d1a046448bcc
Kunal Patel <kunalpatelx92@gmail.com>
2016-04-13 11:41:56 -0500

    Add Pseudocode for secret_agents.rb file

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,27 @@
+# Release 2: Build an Encryption Program
+
+=begin
+
+PSEUDOCODE
+
+Encrypt Method-
+Input: String
+Output: String
+Steps:
+1. Define a method with one parameter (string)
+2. Declare a increment variable (counter) & set it to 0
+3. WHILE counter is less than string.length
+4. Take string[counter] and use the .next method to advance the character to the next character
+5. Increment the counter by 1
+6. End the loop
+7. End the method
+
+
+Decrypt Method-
+Input:
+Output:
+Steps:
+
+=end
+
+# Initial Solution
