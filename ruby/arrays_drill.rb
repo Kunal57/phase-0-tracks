4ee9e084028fdec1f15ee74ff4787891c29020c1
Kunal Patel <kunalpatelx92@gmail.com>
2016-04-19 17:06:26 -0500

    Complete Challenge 5.2 w/ PAIR

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,41 +1,42 @@
 # 5.2 Mandatory Pairing: Arrays
 
+
 # Release 1: Explore the Docs
 
-leaders = []
-p leaders
+nba_players = []
+p nba_players
 
-leaders.push("Elon Musk","Steve Jobs","Warren Buffet", "Mark Cuban", "Bill Gates")
-p leaders
+nba_players.push("Steph Curry","Lebron James","Dwayne Wade", "Chris Paul","Kyrie Irving")
+p nba_players
 
-leaders.delete_at(2)
-p leaders
+nba_players.delete_at(2)
+p nba_players
 
-leaders.insert(2, "Tom Brady")
-p leaders
+nba_players.insert(2, "Kwahi Leonard")
+p nba_players
 
-leaders.shift
-p leaders
+nba_players.shift
+p nba_players
 
-leaders.include?("Steve Jobs")
-p "Leaders contains Steve Jobs: #{leaders.include?("Steve Jobs")}"
+puts "Is \"Lebron James\" in the Array?"
+p nba_players.include?("Lebron James")
 
-other_leaders = ["Kobe Bryant", "Tom Hardy", "Bruce Wayne"]
+more_players = ["Paul George", "Anthony Davis", "Damian Lillard"]
+p more_players
 
-role_models = leaders.concat(other_leaders)
-p role_models
+all_stars = nba_players.concat(more_players)
+p all_stars
+
 
 # Release 2: Use Arrays in Methods
 
 def build_array(param1, param2, param3)
-	array = [param1, param2, param3]
+	[param1, param2, param3]
 end
 
-def add_to_array(array, addition)
-	array << addition
+def add_to_array(array, value)
+	array << value
 end
 
 p build_array(1, "two", nil)
-
-p add_to_array([], "a")
 p add_to_array(["a", "b", "c", 1, 2], 3)
