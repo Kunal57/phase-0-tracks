dceb7a960a0fb1515d429d05ce39743104275268
Kunal Patel <kunalpatelx92@gmail.com>
2016-04-26 17:36:35 -0500

    Complete Release 3 for Challenge 6.5

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,39 @@
 # 6.5 Modules
 
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		words + "!!!" + " :)"
 	end
 end
 
+	class Sports_fan
+		include Shout
+	end
+
+	class Professor
+		include Shout
+	end
+
 # DRIVER CODE
 
-emotions = Shout
+conner = Sports_fan.new
+p conner.yelling_happily("Go Cubs Go!")
+p conner.yell_angrily("The Sox Suck!")
 
-p emotions.yell_angrily("Hello")
-p emotions.yelling_happily("Hello")
+victor = Professor.new
+p victor.yelling_happily("That's the right answer")
+p victor.yell_angrily("Get Out!")
