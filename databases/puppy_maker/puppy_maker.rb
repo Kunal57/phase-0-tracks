c89f81c96c2090753dcffeadf46327c6a2a2867e
Kunal Patel <kunalpatelx92@gmail.com>
2016-05-11 10:42:41 -0500

    Create puppy_maker file & puppies database

--- databases/puppy_maker/puppy_maker.rb
+++ databases/puppy_maker/puppy_maker.rb
@@ -13,16 +13,39 @@ require 'faker'
 
 # Create SQLite3 database
 db = SQLite3::Database.new("puppies.db")
+db.results_as_hash = true
 
 # Learn about fancy string delimiters
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS puppies (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT
+	);
+SQL
 
 # Create a puppy table (if it's not there already)
+db.execute(create_table_cmd)
 
 # Add a test kitten
+#  db.execute("INSERT INTO puppies (name, age) VALUES ('Bob',10)")
 
 # Explore ORM by retrieving data
 
 # Add LOOOOTS of puppies!
 # SO. MANY. PUPPIES.
-
 # PuppyExplosion
+
+def create_puppy(database, name, age)
+	database.execute("INSERT INTO puppies (name, age) VALUES (?,?)", [name, age])
+end
+
+10000.times do
+	create_puppy(db, Faker::Name.name, Random.rand(10))
+end
+
+
+# puppies = db.execute("SELECT * FROM puppies;")
+# puppies.each do |puppy|
+# 	puts "#{puppy['name']} is #{puppy['age']}"
+# end
