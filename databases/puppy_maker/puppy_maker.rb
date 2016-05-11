6fddfc2407b0cf83285b20295d8ef44c2513606b
Kunal Patel <kunalpatelx92@gmail.com>
2016-05-11 09:54:36 -0500

    Add puppy_maker.rb and puppies.db files

---
+++ databases/puppy_maker/puppy_maker.rb
@@ -0,0 +1,28 @@
+# OPERATION PUPPY EXPLOSION!
+
+# Talk about Object Relational Mapping (ORM)
+# Taking relational data (tables that are related to
+# each other through primary and foreign keys) and use it in an object oriented language like Ruby. Basically how do
+# we make this data in sql and table format useful in a 
+# scripting language like Ruby or Python. 
+
+# Require gems
+# A gem is pre-written ruby that you can simply require (Ex: Rspec).
+require 'sqlite3'
+require 'faker'
+
+# Create SQLite3 database
+db = SQLite3::Database.new("puppies.db")
+
+# Learn about fancy string delimiters
+
+# Create a puppy table (if it's not there already)
+
+# Add a test kitten
+
+# Explore ORM by retrieving data
+
+# Add LOOOOTS of puppies!
+# SO. MANY. PUPPIES.
+
+# PuppyExplosion
