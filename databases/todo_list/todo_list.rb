3eb3f8cdac87bbee1dbcdbc73988bbd4b35b9913
Kunal Patel <kunalpatelx92@gmail.com>
2016-05-11 10:53:21 -0500

    Add initial todo_list file

---
+++ databases/todo_list/todo_list.rb
@@ -0,0 +1,17 @@
+# ToDo List
+
+# Create a ToDo List program with Ruby
+
+=begin
+
+PSEUDOCODE
+Input: String
+Output: Updated List
+Steps:
+1. 
+
+=end
+
+# Initial Solution
+
+# Create a SQLite3 database that stores the information
