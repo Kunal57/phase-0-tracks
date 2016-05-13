a2623b390dc19f6718400f1f3f81a92d5a254df9
Kunal Patel <kunalpatelx92@gmail.com>
2016-05-13 18:55:54 -0500

    Add schedule database & functionality to view an existing schedule

--- databases/scheduler/scheduler.rb
+++ databases/scheduler/scheduler.rb
@@ -1,9 +1,46 @@
 require 'sqlite3'
 
+# SQLite3 Database
+db = SQLite3::Database.new("schedule.db")
+
+create_schedules_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS schedules (
+		id INTEGER PRIMARY KEY,
+		day VARCHAR(255),
+		'1:00AM' VARCHAR(255),
+		'2:00AM' VARCHAR(255),
+		'3:00AM' VARCHAR(255),
+		'4:00AM' VARCHAR(255),
+		'5:00AM' VARCHAR(255),
+		'6:00AM' VARCHAR(255),
+		'7:00AM' VARCHAR(255),
+		'8:00AM' VARCHAR(255),
+		'9:00AM' VARCHAR(255),
+		'10:00AM' VARCHAR(255),
+		'11:00AM' VARCHAR(255),
+		'12:00PM' VARCHAR(255),
+		'1:00PM' VARCHAR(255),
+		'2:00PM' VARCHAR(255),
+		'3:00PM' VARCHAR(255),
+		'4:00PM' VARCHAR(255),
+		'5:00PM' VARCHAR(255),
+		'6:00PM' VARCHAR(255),
+		'7:00PM' VARCHAR(255),
+		'8:00PM' VARCHAR(255),
+		'9:00PM' VARCHAR(255),
+		'10:00PM' VARCHAR(255),
+		'11:00PM' VARCHAR(255),
+		'12:00AM' VARCHAR(255)
+	);
+SQL
+
+db.execute(create_schedules_table_cmd)
+
+# Schedule Class
 
 class Schedule
 
-	def initialize(date)
+	def initialize(database, date)
 		@date = date
 		@schedule = {
 									"1:00AM" => "Sleep",
@@ -32,6 +69,8 @@ class Schedule
 									"12:00AM" => "Sleep",
 								}
 		@hours = ["1:00AM","2:00AM","3:00AM","4:00AM","5:00AM","6:00AM","7:00AM","8:00AM","9:00AM","10:00AM","11:00AM","12:00PM","1:00PM","2:00PM","3:00PM","4:00PM","5:00PM","6:00PM","7:00PM","8:00PM","9:00PM","10:00PM","11:00PM","12:00AM","1:00AM","2:00AM","3:00AM","4:00AM","5:00AM","6:00AM","7:00AM","8:00AM","9:00AM","10:00AM","11:00AM","12:00PM","1:00PM","2:00PM","3:00PM","4:00PM","5:00PM","6:00PM","7:00PM","8:00PM","9:00PM","10:00PM","11:00PM","12:00AM"]
+		
+		database.execute("INSERT INTO schedules (day, '1:00AM', '2:00AM', '3:00AM', '4:00AM', '5:00AM', '6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM', '8:00PM', '9:00PM', '10:00PM', '11:00PM', '12:00AM') VALUES (?,'Sleep','Sleep','Sleep','Sleep','Wake/Read','Sit-ups/Shower','Medidate/Puja/Breakfast','Plan the Day/Read','Code','Code','Code','Code','Lunch','Exercise','Code','Code','Code','Code','Dinner','Brush/Examine the Day','Read/Social Media','Sleep','Sleep','Sleep')", [date])
 	end
 
 	def update(time, new_task)
@@ -101,14 +140,69 @@ end
 
 # DRIVER CODE
 
-puts "Create New List ('N') or Select Existing List ('E')"
+# METHOD TO VIEW EXISTING SCHEDULE IN PROPER FORMAT
+def view_existing_list(database, date)
+	time1AM = database.execute("SELECT schedules.'1:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time2AM = database.execute("SELECT schedules.'2:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time3AM = database.execute("SELECT schedules.'3:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time4AM = database.execute("SELECT schedules.'4:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time5AM = database.execute("SELECT schedules.'5:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time6AM = database.execute("SELECT schedules.'6:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time7AM = database.execute("SELECT schedules.'7:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time8AM = database.execute("SELECT schedules.'8:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time9AM = database.execute("SELECT schedules.'9:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time10AM = database.execute("SELECT schedules.'10:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time11AM = database.execute("SELECT schedules.'11:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+	time12PM = database.execute("SELECT schedules.'12:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time1PM = database.execute("SELECT schedules.'1:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time2PM = database.execute("SELECT schedules.'2:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time3PM = database.execute("SELECT schedules.'3:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time4PM = database.execute("SELECT schedules.'4:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time5PM = database.execute("SELECT schedules.'5:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time6PM = database.execute("SELECT schedules.'6:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time7PM = database.execute("SELECT schedules.'7:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time8PM = database.execute("SELECT schedules.'8:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time9PM = database.execute("SELECT schedules.'9:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time10PM = database.execute("SELECT schedules.'10:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time11PM = database.execute("SELECT schedules.'11:00PM' FROM schedules WHERE schedules.day=(?)",[date])
+	time12AM = database.execute("SELECT schedules.'12:00AM' FROM schedules WHERE schedules.day=(?)",[date])
+
+	puts "\n1:00AM - #{time1AM[0][0]}"
+	puts "2:00AM - #{time2AM[0][0]}"
+	puts "3:00AM - #{time3AM[0][0]}"
+	puts "4:00AM - #{time4AM[0][0]}"
+	puts "5:00AM - #{time5AM[0][0]}"
+	puts "6:00AM - #{time6AM[0][0]}"
+	puts "7:00AM - #{time7AM[0][0]}"
+	puts "8:00AM - #{time8AM[0][0]}"
+	puts "9:00AM - #{time9AM[0][0]}"
+	puts "10:00AM - #{time10AM[0][0]}"
+	puts "11:00AM - #{time11AM[0][0]}"
+	puts "12:00PM - #{time12PM[0][0]}"
+	puts "1:00PM - #{time1PM[0][0]}"
+	puts "2:00PM - #{time2PM[0][0]}"
+	puts "3:00PM - #{time3PM[0][0]}"
+	puts "4:00PM - #{time4PM[0][0]}"
+	puts "5:00PM - #{time5PM[0][0]}"
+	puts "6:00PM - #{time6PM[0][0]}"
+	puts "7:00PM - #{time7PM[0][0]}"
+	puts "8:00PM - #{time8PM[0][0]}"
+	puts "9:00PM - #{time9PM[0][0]}"
+	puts "10:00PM - #{time10PM[0][0]}"
+	puts "11:00PM - #{time11PM[0][0]}"
+	puts "12:00AM - #{time12AM[0][0]}"
+end
+
+
+puts "Create New Schedule ('N') or Select Existing Schedule ('E')"
 list = gets.chomp
 
 if list.downcase == "n"
 	puts "Enter date:"
 	date = gets.chomp
-	schedule = Schedule.new(date)
+	schedule = Schedule.new(db, date)
 	
+	# NEW SCHEDULES
 	action = ""
 	until action.downcase == 'q'
 	puts "\n1. View List (V)\n2. Update List (U)\n3. Morning Routine (W)\n4. Prepare for Bed (B) \n5. 7 Hours of Sleep (S) \n6. Set List to Default (D) \n7. Quit (Q)"
@@ -142,9 +236,48 @@ if list.downcase == "n"
 			puts "\nInvalid input."
 		end
 	end
+
+# EXISTING SCHEDULES	
 elsif list.downcase == "e"
-	puts "Enter date:"
+	puts "\nSchedules in database:"
+	puts db.execute("SELECT schedules.day FROM schedules")
+
+	puts "\nEnter schedule date:"
 	date = gets.chomp
+
+	action = ""
+		until action.downcase == 'q'
+		puts "\n1. View List (V)\n2. Update List (U)\n3. Morning Routine (W)\n4. Prepare for Bed (B) \n5. 7 Hours of Sleep (S) \n6. Set List to Default (D) \n7. Quit (Q)"
+		action = gets.chomp
+			if action.downcase == "v"
+				view_existing_list(db, date)
+			elsif action.downcase == "u"
+				puts "\nUpdate Time:"
+				time = gets.chomp
+				puts "Update Task:"
+				new_task = gets.chomp
+				schedule.update(time, new_task)
+			elsif action.downcase == "w"
+				puts "\nWake Up Time:"
+				wake_time = gets.chomp
+				schedule.wake(wake_time)
+			elsif action.downcase == "b"
+				puts "\nPrepare for Sleep Time:"
+				bed = gets.chomp
+				schedule.bed(bed)
+			elsif action.downcase == "s"
+				puts "\nSleep Time:"
+				sleep = gets.chomp
+				schedule.sleep(sleep)
+			elsif action.downcase == "d"
+				schedule.default
+				puts "List successfully set to default."
+			elsif action.downcase == "q"
+				break
+			else
+				puts "\nInvalid input."
+			end
+		end
 else
 	puts "Invalid input."
 end 
